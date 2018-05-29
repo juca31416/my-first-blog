0b40f902e71a68452319254087d0968f425a96e5
daw <jgonzalez20@uabc.edu.mx>
2018-05-28 19:25:36 -0700

    Actualizaciones.

--- blog/views.py
+++ blog/views.py
@@ -17,8 +17,7 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
     	    post = form.save(commit=False)
-    	    post.author = request.user
-    	    post.published_date = timezone.now()
+    	    post.author = request.user    	    
     	    post.save()
     	    return redirect('post_detail', pk=post.pk)
     else:
@@ -30,10 +29,12 @@ def post_edit(request, pk):
 		form = PostForm(request.POST, instance=post)
 		if form.is_valid():
 			post = form.save(commit=False)
-			post.author = request.user
-			post.published_date = timezone.now()
+			post.author = request.user			
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts': posts})
