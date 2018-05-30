4c4634a9ef3a6d0ead737b2e05265ca1b23898f4
daw <jgonzalez20@uabc.edu.mx>
2018-05-29 19:59:01 -0700

    New commit

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
+from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
 
@@ -45,7 +45,7 @@ def post_edit(request, pk):
 @login_required
 def post_draft_list(request):
 	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-	return render(request, 'post_draft_list.html', {'posts': posts})
+	return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 @login_required
 def post_publish(request, pk):
@@ -67,7 +67,19 @@ def add_comment_to_post(request, pk):
 			comment = form.save(commit=False)
 			comment.post = post
 			comments.save()
-			return redirect('post detail', pk=post.pk)
+			return redirect('post_detail', pk=post.pk)
 	else:
 		form = CommentForm()
 	return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+	comment = get_object_or_404(Comment, pk=pk)
+	comment.delete()
+	return redirect('post_detail', pk=comment.post.pk)
